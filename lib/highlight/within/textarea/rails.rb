2fa6652643c2b03e89821a79a0b3c3b16cb5591f
Ukaza Perdana <ukaza.putro@jurnal.id>
2017-11-03 13:18:28 +0700

    Yowza!

---
+++ lib/highlight/within/textarea/rails.rb
@@ -0,0 +1,12 @@
+require "highlight/within/textarea/rails/version"
+
+module Highlight
+  module Within
+    module Textarea
+      module Rails
+        class Engine < ::Rails::Engine
+        end
+      end
+    end
+  end
+end
